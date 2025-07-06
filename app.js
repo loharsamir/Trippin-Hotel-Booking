a02adc668f8b8664a1e672043bccc92b1154ea21
“loharsamir” <samirlohar2403@gmail.com>
2025-07-06 19:55:14 +0530

    db connection fix

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-const dbUrl ="mongodb://127.0.0.1:27017/wanderlust";
-// const dbUrl=process.env.ATLASDB_URL;
+//const dbUrl ="mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl=process.env.ATLASDB_URL;
 
 const store= MongoStore.create({
     mongoUrl: dbUrl,

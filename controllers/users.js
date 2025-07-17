e28908a50ecc456392fecdf9a2108a1ac86efd0a
“loharsamir” <samirlohar2403@gmail.com>
2025-07-18 01:23:33 +0530

    change the name and logo

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup = async(req,res)=>{
         if(err){
             return next(err);
         }
-        req.flash("success","Welcome to Wanderlust");
+        req.flash("success","Welcome to Trippin");
         res.redirect("/listings")
     })
     
@@ -28,7 +28,7 @@ module.exports.renderLoginForm = (req,res)=>{
     res.render("users/login.ejs")
 };
 module.exports.login = async(req,res)=>{
-    req.flash("success","Welcome back to Wanderlust!");
+    req.flash("success","Welcome back to Trippin!");
     let redirectUrl=res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
 };
